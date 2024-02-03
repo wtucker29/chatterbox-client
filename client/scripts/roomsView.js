a7ed0503db2377b4b68854474a66984a2216864a
William Tucker <william.tucker29@gmail.com>
2024-02-02 21:47:03 -0600

    Created initialize, renderRoom, handleClick, and template functions. Currently adds option element when room is added to drop down. Creates a prompt to enter room name and puts it in list. Renders the room list when added.

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -1,6 +1,7 @@
 // RoomsView is an object which controls the DOM elements
 // responsible for displaying and selecting rooms.
 
+//add options to .room select
 var RoomsView = {
 
   $button: $('#rooms button'),
@@ -9,6 +10,7 @@ var RoomsView = {
   initialize: function() {
     // TODO: Perform any work which needs to be done
     // when this view loads.
+    this.$button.on('click', this.handleClick);
   },
 
   render: function() {
@@ -17,6 +19,11 @@ var RoomsView = {
 
   renderRoom: function(roomname) {
     // TODO: Render out a single room.
+    // Create room node and call roomTemplate within it
+    var obj = {roomname: roomname};
+    var $roomNode = $(this.roomTemplate(obj));
+    // Append room node to options id
+    this.$select.append($roomNode);
   },
 
   handleChange: function(event) {
@@ -25,6 +32,19 @@ var RoomsView = {
 
   handleClick: function(event) {
     // TODO: Handle the user clicking the "Add Room" button.
-  }
+    var roomName = prompt('Enter new room name: ');
+    if (roomName) {
+      Rooms.add(roomName);
+      RoomsView.renderRoom(roomName);
+    }
+    // $('#rooms button').on('click', function() {
+    //   var room = $(this).text();
+    //   Rooms.add(room);
+    // });
+  },
+
+  roomTemplate: _.template(`
+      <option class="roomname"><%- roomname %></option>
+  `)
 
 };
