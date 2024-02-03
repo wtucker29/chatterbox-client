650dc3c9094c8e4e99a63a4485d8dc1520341516
William Tucker <william.tucker29@gmail.com>
2024-02-02 21:46:18 -0600

    Created add and contains functions. Adds rooms to drop down list and checks to see if list already contains room name

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -5,9 +5,21 @@
 var Rooms = {
 
   // TODO: Define how you want to store the list of rooms
-  _data: null,
+  _data: {},
 
   // TODO: Define methods which allow you to add rooms, update the list,
   // mark a room as selected, etc.
+  add: function (roomName) {
+    if (this._data[roomName] === undefined) {
+      this._data[roomName] = roomName;
+    }
+  },
 
+  contains: function(roomName) {
+    if (this._data[roomName] !== undefined) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
