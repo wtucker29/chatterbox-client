ea7cc2a2d7d390df7e4b3672c2f2d4952742d851
William Tucker <william.tucker29@gmail.com>
2024-02-02 21:41:26 -0600

    Started to build handleSubmit function. Working on implementing Parse.create()

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -16,7 +16,14 @@ var FormView = {
 
     // TODO: Currently, this is all handleSubmit does.
     // Make this function actually send a message to the Parse API.
-  
+    var message = {
+      text: FormView.$form.find('input[type=text]').val(),
+      username: App.username,
+      roomname: $('#rooms select')
+    };
+
+    Parse.create(message, successCB, errorCB);
+
     console.log('click!');
   },
 
