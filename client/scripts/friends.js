a64a383ba21ce18cdc824bd6dd5e0bacc58704d8
William Tucker <william.tucker29@gmail.com>
2024-02-02 21:42:08 -0600

    Created toggleStatus and add functions to add users to friends list when clicking on their name

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -5,9 +5,23 @@
 var Friends = {
   // TODO: Define how you want to store your list of friends.
 
-  _data: null,
+  _data: {},
 
   // TODO: Define methods which allow you to add, toggle,
   // and check the friendship status of other users.
 
+  toggleStatus: function(username) {
+    if (this._data[username] === undefined) {
+      this.add(username);
+    }
+    if (this._data[username] === true) {
+      this._data[username] === false;
+    } else {
+      this._data[username] === true;
+    }
+  },
+
+  add: function(username) {
+    this._data[username] = true;
+  }
 };
