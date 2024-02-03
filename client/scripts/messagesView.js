7de60a9b4cefc39ada6c3be7351004cdf6940b2e
William Tucker <william.tucker29@gmail.com>
2024-02-02 21:44:12 -0600

    Updated initialize, render, renderMessage, and handleClick functions. Messages are now appended to html body. Clicking usernames adds them to friends list

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -8,19 +8,35 @@ var MessagesView = {
   initialize: function() {
     // TODO: Perform any work which needs to be done
     // when this view loads.
+    // MessagesView.$chats.on('click', '.message', MessagesView.handleClick);
+    this.$chats.on('click', '.username', this.handleClick);
+
+    // this.render();
   },
 
-  render: function() {
+  render: function(messages) {
     // TODO: Render _all_ the messages.
+    // Iterate through array and call renderMessage on each item in array
+    messages.forEach(message => this.renderMessage(message));
   },
 
-  renderMessage: function(message) {
+  renderMessage: function(message) { //message is an object with Username:, text:, roomname:
     // TODO: Render a single message.
+    // to give us nodes to append to chats
+    var $messageNode = $(MessageView.render(message));
+    //append to chats
+    debugger;
+    this.$chats.append($messageNode);
   },
 
   handleClick: function(event) {
     // TODO: handle a user clicking on a message
     // (this should add the sender to the user's friend list).
+    //call toggleStatus
+    $('.username').on('click', function() {
+      var user = $(this).text();
+      Friends.toggleStatus(user);
+    });
   }
 
 };
