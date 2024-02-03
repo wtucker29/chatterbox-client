85ae656e7669c599b971ec6ea9aafcc0a918c6df
William Tucker <william.tucker29@gmail.com>
2024-02-02 21:43:06 -0600

    Updated template in the render function to work properly. Adds parent div element with two child div elements for the Message Box functionality

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -6,12 +6,10 @@ var MessageView = {
   // here: https://underscorejs.org/#template.
   // TODO: Update this template accordingly.
   render: _.template(`
-      <!--
-      <div class="chat">
-        <div class="username"></div>
-        <div></div>
+      <div class="message">
+        <div class="username"><%- username %></div>
+        <div class="text"><%- text %></div>
       </div>
-      -->
     `)
-
 };
+//method to append to messagesView #chats
